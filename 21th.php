cecb7ab5e7d342adb0eb0b8cf20d9fc79447db3a
Paul Okeke <donpaul120@gmail.com>
2014-02-24 16:08:00 +0000

    Done today

---
+++ 21th.php
@@ -0,0 +1,20 @@
+//Day 2
+
+<?php 
+Feburary 21,02, 2014.
+
+//controllers provide a number of methods which are called actions
+
+//Actions are method in controllers which handles request--
+//Actions are responsible for interpreting request and giving back responses
+
+
+//beforeFilter
+//beforeRender
+//AfterFilter
+
+//using the set(); method of the controller
+//using the render() method of the controller to render a page
+
+
+?>
